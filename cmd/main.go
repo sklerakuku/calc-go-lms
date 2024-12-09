b45f8c0eee7ab05dc2a7a27ce1fcb0fa71e8fc92
sklerakuku <54020115+sklerakuku@users.noreply.github.com>
2024-12-09 15:52:02 +1000

    int commit

--- cmd/main.go
+++ cmd/main.go
@@ -6,6 +6,6 @@ import (
 
 func main() {
 	app := application.New()
-	app.Run()
-	//app.RunServer()
+	//app.Run()
+	app.RunServer()
 }
---
+++ internal/application/application_test.go
@@ -0,0 +1,15 @@
+package application_test
+
+/*
+
+прописать тесты для handler-ов
+
+прописать конкретные ошибки в файле errors.go (package calculation, var(...))
+
+	ErrInvalidExprassion = errors.New("invalide expration")
+
+прописать файл readme.md
+
+
+
+*/
